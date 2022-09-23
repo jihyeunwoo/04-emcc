cfbde054756e067e35017339279138ee7324618f
ji hyeon woo <908dkwmdk@naver.com>
2022-09-23 10:12:28 +0900

    detail position / script ing...
    디테일부분이랑 스크립트부분 진행중...

--- script/main.js
+++ script/main.js
@@ -1,3 +1,13 @@
-$(function(){
-	
+$(function() {
+   // $ : 찾아라
+   // click(function(){....}) : 클릭하면 실행되는 메소드
+   // 
+   $("#open").click(function() {
+      $(".item-detail").removeClass("d-none");   //.removeClass() : 클래스 선택자 제거 메소드 ↔ .addClass()
+      //$(".item-detail").show("slow");   //ex> 300
+   })
+   $("#close").click(function() {
+      $(".item-detail").addClass("d-none");   //.addClass() : 클래스 선택자 추가 메소드 ↔ .removeClass()
+      //$(".item-detail").hide("fast");
+   })
 })
